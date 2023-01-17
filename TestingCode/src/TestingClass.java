db1250fecf706e41422a3e52aa37c6e937781d54
krsna <krsna@localhost>
2023-01-17 21:08:44 +0530

    First commit

---
+++ TestingCode/src/TestingClass.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TestingClass {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the key value to search: ");
+		int key=scan.nextInt();
+		int[] a= {10,30,40,50,60,70,80,90,110,150};
+		int low=0,high=a.length;
+		
+		while(low<=high) {
+			int mid=(low+high)/2;
+			
+			if(key==a[mid]) {
+				System.out.println("key found at "+mid+" index");
+				break;
+			}
+			else if(key<a[mid]) {
+				high=mid-1;
+			}
+			else {
+				low=mid+1;
+			}
+		}
+		if(low>high) {
+			System.out.println("key not found");
+		}
+		
+	}
+
+}
